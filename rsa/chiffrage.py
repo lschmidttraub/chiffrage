975d9a40868a9a6450876546e0c1d27a348eb1d6
Leo Schmidt-Traub <70472063+spicyboi26@users.noreply.github.com>
2022-12-09 16:24:24 +0100

    Exponentiation rapide

--- rsa/chiffrage.py
+++ rsa/chiffrage.py
@@ -1,12 +1,21 @@
+def exponentiation_rapide(base, exp, modulo):
+    res=1
+    while exp!=0:
+        if exp%2:
+            res*=base
+        exp//=2
+        base=base**2%modulo
+    return res%modulo
+
 def chiffrer(message, clef, n):
     res = ""
     for char in message:
-        res+=str((ord(char)**clef)%n)+" "
+        res+=str(exponentiation_rapide(ord(char), clef, n))+" "
     return (res)
 
 def dechiffrer(message, clef, n):
     res = ""
     for nb in message.split(" "):
         if nb!="":
-            res=res+chr((int(nb)**clef)%n)
+            res=res+chr(exponentiation_rapide(int(nb), clef, n))
     return res
