f52c18f9ebd0a14f9b00f8ff131f6ae26fb506b1
spicyboi26 <leo.schmidttraub@gmail.com>
2022-12-10 09:01:42 +0100

    Ajout des nouveaux dossiers

---
+++ chiffrement_RSA/nombres_premiers.py
@@ -0,0 +1,21 @@
+def premier(n):
+    if n <= 3:
+        return n > 1
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    limite = int(n**0.5)
+    for i in range(5, limite+1, 6):
+        if n % i == 0 or n % (i+2) == 0:
+            return False
+    return True
+
+def prochain_nb_premier(n):
+    if (not n%2) and (n != 2):
+        n += 1
+    if premier(n):
+        n += 2
+    while True:
+        if premier(n):
+            break
+        n += 2
+    return n
