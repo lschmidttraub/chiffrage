4d6268bf563a3f79205ddf5be8fea0f9b69d4914
Leo Schmidt-Traub <70472063+spicyboi26@users.noreply.github.com>
2023-01-06 16:25:57 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -11,7 +11,8 @@ elif choix == "1":
 elif choix == "2":
   
 elif choix == "3":
-  n, clef_publique, clef_prive = generer_clefs(10**4)
+  maximum = int(input("Choisissez une valeur maximale pour les nombres premiers p et q (un nombre plus grand est plus sécurisé mais prend plus longtemps à calculer): ")
+  n, clef_publique, clef_prive = generer_clefs(maximum)
   fonction = input("Choisissez une fonctionalité (Entrez q pour retourner au menu): \n 1) Chiffrer ou déchiffrer un message \n 2) Chiffrer ou déchiffrer le contenu du fichier message.txt \n")
   if option == "q":
   elif option == "1":
@@ -19,9 +20,11 @@ elif choix == "3":
   elif option == "2":
     message = 
   
-  option2 = input("Voulez vous chiffrer(1) ou déchiffrer (2) ce message")
+  option2 = input("Voulez vous chiffrer(1) ou déchiffrer(2) ce message")
   if option2 == "1":
     res = rsa.chiffrer(message, clef_publique, n)
   elif option2 == "2":
     res = rsa.chiffrer(message, clef_privee, n)
-  
+  print("Voici le résultat:\n", message)
+  stocker = input("Voulez-vous stocker ce résultat dans le fichier message.txt? (o/n) ")
+  if(stocker == "o"):
