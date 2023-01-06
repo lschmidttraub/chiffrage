8ee5b6a427edbfd331fe71f6622c3e53c235dd75
Leo Schmidt-Traub <70472063+spicyboi26@users.noreply.github.com>
2023-01-06 15:16:48 +0100

    Update chiffrage.py

--- chiffrement_RSA/chiffrage.py
+++ chiffrement_RSA/chiffrage.py
@@ -1,4 +1,8 @@
 def exponentiation_rapide(base, exp, modulo):
+    '''
+    Entrée: une base (int), un exposant (int) et un modulo (int)
+    Sortie: la valeur (base**exponent)%modulo
+    '''
     res=1
     while exp!=0:
         if exp%2:
@@ -8,12 +12,20 @@ def exponentiation_rapide(base, exp, modulo):
     return res%modulo
 
 def chiffrer(message, clef, n):
+    '''
+    Entrée: une chaîne de caractères (le message à chiffrer), un entier positif (la clef) et un modulo n 
+    Sortie: le message chiffré
+    '''
     res = ""
     for char in message:
         res+=str(exponentiation_rapide(ord(char), clef, n))+" "
     return (res)
 
 def dechiffrer(message, clef, n):
+    '''
+    Entrée: une chaîne de caractères (le message chiffré), un entier positif (la clef) et  
+    Sortie: le message dechiffré
+    '''
     res = ""
     for nb in message.split(" "):
         if nb!="":
