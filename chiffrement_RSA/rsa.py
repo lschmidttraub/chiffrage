7ab82be0b4f28903eb6af6c2182dba7d880252e9
Leo Schmidt-Traub <70472063+spicyboi26@users.noreply.github.com>
2023-01-06 15:22:07 +0100

    Update rsa.py

--- chiffrement_RSA/rsa.py
+++ chiffrement_RSA/rsa.py
@@ -10,22 +10,26 @@ from arithmetique_modulaire import *
 from chiffrage import *
 
 
-def generer_clefs():
-    p = secrets.randbelow(10**3)
-    q = secrets.randbelow(10**3)
+def generer_clefs(maximum, clef_publique=2**16+1):
+    '''
+    Entrée: une valeur maximale pour les nombres premiers p et q, une clef publique
+    Sortie: un modulo n, la clef publique et la clef privée
+    '''
+    p = secrets.randbelow(maximum)
+    q = secrets.randbelow(maximum)
     p=prochain_nb_premier(p)
     q=prochain_nb_premier(q)
-
     n=p*q
     lam=ppcm(p-1,q-1)
-    clef_public=2**16+1
-    clef_prive=coefficient_bezout(clef_public, lam)
+    clef_prive=coefficient_bezout(clef_publique, lam)
     if clef_prive<0:
         clef_prive+=lam
-    return n, clef_public, clef_prive
+    return n, clef_publique, clef_prive
 
-n, clef_public, clef_prive = generer_clefs()
+"""
+n, clef_publique, clef_prive = generer_clefs(10**4)
 
 message="Inshallah ça marche !/.,_'é#@"
-m=chiffrer(message, clef_public, n)
+m=chiffrer(message, clef_publique, n)
 assert dechiffrer(m, clef_prive, n) == message
+"""
