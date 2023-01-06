67b5a7a0c520c8bc6c7a4891ced02edea6da9726
Leo Schmidt-Traub <70472063+spicyboi26@users.noreply.github.com>
2023-01-06 15:03:49 +0100

    Update arithmetique_modulaire.py

--- chiffrement_RSA/arithmetique_modulaire.py
+++ chiffrement_RSA/arithmetique_modulaire.py
@@ -1,4 +1,8 @@
 def pgcd(a,b):
+    '''
+    Entrée: deux entiers positifs a et b
+    Sortie: le Plus Grand Diviseur Commun (PGCD) de a et b
+    '''
     a,b=max(a,b), min(a,b)
     r=a%b
     if r==0:
@@ -6,9 +10,17 @@ def pgcd(a,b):
     return pgcd(b,r)
 
 def ppcm(a,b):
+    '''
+    Entrée: deux entiers positifs a et b
+    Sortie: le Plus Petit Commun Multiple (PPCM) de a et b
+    '''
     return int(a*b/pgcd(a,b))
 
 def coefficient_bezout(a,b):
+    '''
+    Entrée: deux entiers positifs a et b premiers entre eux
+    Sortie: le premier coefficient Bézout
+    '''
     r1, r2 = a,b
     s1, s2 = 1,0
     # t1, t2 = 0,1
