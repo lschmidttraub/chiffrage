a18e70be9c50f0c83f60601a3d616ad1390ef6d1
Leo Schmidt-Traub <70472063+spicyboi26@users.noreply.github.com>
2023-01-06 15:42:34 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,8 +1,15 @@
-from chiffrement_RSA.rsa import *
-from chiffre_de_Cesar.Cesar import *
-from chiffre_de_Vigenere.Vigenere import *
+from chiffrement_RSA import rsa
+from chiffre_de_Cesar import Cesar
+from chiffre_de_Vigenere import Vigenere
 
-choix = input("Choisissez une méthode de cryptage: \n 1: Chiffre de César \n 2: Chiffre de Vigenère \n 3: Chiffrement RSA\n")
+choix = input("Choisissez une méthode de cryptage (Entrez q pour quitter): \n 1: Chiffre de César \n 2: Chiffre de Vigenère \n 3: Chiffrement RSA\n")
 
-if choix == "1":
+if choix == "q":
+  print("au revoir")
+elif choix == "1":
+  
+elif choix == "2":
+  
+elif choix == "3":
+ 
   
