015558f835c218e33d9e72f8686979b046f7fb5b
Leo Schmidt-Traub <70472063+spicyboi26@users.noreply.github.com>
2023-01-06 16:04:06 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -11,5 +11,7 @@ elif choix == "1":
 elif choix == "2":
   
 elif choix == "3":
- 
+  n, clef_publique, clef_prive = generer_clefs(10**4)
+  fonction = input("Choisissez une fonctionalité (Entrez q pour retourner au menu): \n 1) Chiffrer un message \n 2) Déchiffrer un message \n")
+  
   
